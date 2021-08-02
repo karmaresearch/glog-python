0841f71b5eb4bbf5042050f5c240f41a27b29f7e
Jacopo Urbani <jacopo@cs.vu.nl>
2021-08-02 13:07:02 +0200

    Implemented new procedure to normalize scores

--- src/python/embtopk/embtopktable.py
+++ src/python/embtopk/embtopktable.py
@@ -2,6 +2,7 @@ from pyiterator import PyIterator
 from pytable import PyTable
 
 import torch
+import numpy as np
 from kge.model import KgeModel
 
 class EmbTopKEDBIterator(PyIterator):
@@ -25,7 +26,7 @@ class EmbTopKEDBIterator(PyIterator):
 
 
 class EmbTopKEDBTable(PyTable):
-    def __init__(self, predname, relname, top_k, edb_layer, model : KgeModel):
+    def __init__(self, predname, relname, top_k, edb_layer, model : KgeModel, entities_dict : dict = None):
         super().__init__(predname, 3)
         self.predname = predname
         self.top_k = top_k
@@ -39,10 +40,40 @@ class EmbTopKEDBTable(PyTable):
                 self.rel_id = id
         if self.rel_id is None:
             raise Exception("Relation not found")
-        # Load the entities
-        self.entities_dict = {}
-        for idx, e in enumerate(self.model.dataset.entity_strings()):
-            self.entities_dict[e] = idx
+        if entities_dict is not None:
+            self.entities_dict = entities_dict
+        else:
+            print("Entity map created from scratch. Consider sharing it with other relations to save RAM!")
+            self.entities_dict = {}
+            for idx, e in enumerate(self.model.dataset.entity_strings()):
+                self.entities_dict[e] = idx
+        self.known_answers_sp = model.dataset.index('train_sp_to_o')
+        self.known_answers_po = model.dataset.index('train_po_to_s')
+        triples = model.dataset.load_triples('train')
+        self.so = [(t[0], t[2]) for t in triples if t[1] == self.rel_id]
+        if len(self.so) == 0:
+            # Best possible scores
+            self.known_min_score_s = 0
+            self.known_min_score_o = 0
+        else:
+            # Calculate scores for the best best 'o'
+            idx = np.random.randint(0, len(self.so), 1)[0]
+            chosen_s = self.so[idx][0]
+            s = torch.Tensor([chosen_s, ]).long()
+            p = torch.Tensor([self.rel_id, ]).long()
+            scores = self.model.score_sp(s, p)[0]
+            o = torch.argsort(scores, descending=True)
+            top_k_o = o[:self.top_k]
+            top_k_scores = scores[top_k_o]
+            self.known_min_score_o = top_k_scores[0]
+            # Calculate scores for the best best 's'
+            chosen_o = self.so[idx][1]
+            o = torch.Tensor([chosen_o, ]).long()
+            scores = self.model.score_po(p, o)[0]
+            s = torch.argsort(scores, descending=True)
+            top_k_s = s[:self.top_k]
+            top_k_scores = scores[top_k_s]
+            self.known_min_score_s = top_k_scores[0]
 
     def _is_query_allowed(self, t):
         l = len(t)
@@ -94,13 +125,80 @@ class EmbTopKEDBTable(PyTable):
     def get_n_terms(self) -> int:
         self.n_terms + 1000 # I assume I return scores with three decimal digits of precision
 
-    def _normalize_scores(self, scores):
+    def _normalize_scores_simple(self, t : tuple, scores):
         sum = 0.0
         for s in scores:
             sum += 1 / s
         for idx, s in enumerate(scores):
             scores[idx] = 1 / (s.item()) / sum.item()
         return scores
+
+    def _normalize_scores(self, t : tuple, scores):
+        # Collect up to k max/min scores
+        true_answers = self._get_true_answers(t)
+        if len(true_answers) > 0:
+            true_scores = self._get_answer_scores(t, true_answers)
+        else:
+            if t[0].is_variable():
+                true_scores = self.known_min_score_s
+            else:
+                true_scores = self.known_min_score_o
+
+        false_answers = self._get_false_answers(t)
+        false_scores = self._get_answer_scores(t, false_answers)
+        # Normalise "scores" depending on true_scores and false_scores
+        best_score = torch.max(true_scores).item()
+        assert(best_score <= 0)
+        worst_score = torch.mean(false_scores).item()
+        assert (worst_score <= 0)
+        assert(best_score > worst_score)
+        clamped_scores = torch.clamp(scores, min=worst_score, max=best_score)
+        scores = clamped_scores - worst_score
+        range = best_score - worst_score
+        prob = scores / range
+        return prob
+
+    def _get_answer_scores(self, t : tuple, a : torch.Tensor):
+        if t[0].is_variable():
+            # Retrieve ID of the object
+            value = t[1].get_value()
+            value_id = self.entities_dict[value]
+            o = torch.Tensor([value_id, ]).long()
+            p = torch.Tensor([self.rel_id, ]).long()
+            scores = self.model.score_po(p, o, a)[0]
+        elif t[1].is_variable():
+            # Retrieve ID of the subject
+            value = t[0].get_value()
+            value_id = self.entities_dict[value]
+            s = torch.Tensor([value_id, ]).long()
+            p = torch.Tensor([self.rel_id, ]).long()
+            scores = self.model.score_sp(s, p, a)[0]
+        else:
+            raise Exception("Not implemented")
+        return scores
+
+    def _get_true_answers(self, t : tuple):
+        if t[0].is_variable():
+            # Retrieve ID of the object
+            value = t[1].get_value()
+            value_id = self.entities_dict[value]
+            o = torch.Tensor([value_id, ]).long().item()
+            p = torch.Tensor([self.rel_id, ]).long().item()
+            answers = self.known_answers_po.get([p, o])
+        elif t[1].is_variable():
+            # Retrieve ID of the subject
+            value = t[0].get_value()
+            value_id = self.entities_dict[value]
+            s = torch.Tensor([value_id, ]).long().item()
+            p = torch.Tensor([self.rel_id, ]).long().item()
+            answers = self.known_answers_sp.get([s, p])
+        else:
+            raise Exception("Not implemented")
+        return answers
+
+    def _get_false_answers(self, t : tuple):
+        random_entities = np.random.randint(0, self.n_terms, 10)
+        return torch.from_numpy(random_entities)
 
     def get_iterator(self, t: tuple) -> PyIterator:
         if not self._is_query_allowed(t):
@@ -118,7 +216,7 @@ class EmbTopKEDBTable(PyTable):
             s = torch.argsort(scores, descending=True)
             top_k_s = s[:self.top_k]
             top_k_scores = scores[top_k_s]
-            top_k_scores = self._normalize_scores(top_k_scores)
+            top_k_scores = self._normalize_scores(t, top_k_scores)
             txt_top_k_s = self.model.dataset.entity_strings(top_k_s)
             for idx, e in enumerate(txt_top_k_s):
                 answers.append((e, value, top_k_scores[idx].item()))
@@ -132,7 +230,7 @@ class EmbTopKEDBTable(PyTable):
             o = torch.argsort(scores, descending=True)
             top_k_o = o[:self.top_k]
             top_k_scores = scores[top_k_o]
-            top_k_scores = self._normalize_scores(top_k_scores)
+            top_k_scores = self._normalize_scores(t, top_k_scores)
             txt_top_k_o = self.model.dataset.entity_strings(top_k_o)
             for idx, e in enumerate(txt_top_k_o):
                 answers.append((value, e, top_k_scores[idx].item()))
@@ -145,7 +243,7 @@ class EmbTopKEDBTable(PyTable):
             o = torch.Tensor([value_o_id, ]).long()
             p = torch.Tensor([self.rel_id, ]).long()
             score = self.model.score_spo(s, p, o)
-            # TODO normalize score
+            score = self._normalize_scores(t, score)
             answers.append((value_s, value_o, score[0].item()))
 
         return EmbTopKEDBIterator(answers)
